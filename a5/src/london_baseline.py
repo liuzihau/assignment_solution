850b93464db2123af7ca69ac51cb3e77973b1f7c
Nick <pokerliuzihau@gmail.com>
2022-09-25 17:57:10 +0800

    add answer

--- a5/src/london_baseline.py
+++ a5/src/london_baseline.py
@@ -2,3 +2,16 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+from tqdm import tqdm
+import utils
+correct = 0
+total = 0
+predictions = []
+for line in tqdm(open('./birth_dev.tsv', encoding='utf-8')):
+    x = line.split('\t')[0]
+    x = x + '⁇'
+    pred = "London"
+    predictions.append(pred)
+total, correct = utils.evaluate_places('./birth_dev.tsv', predictions)
+if total > 0:
+    print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
